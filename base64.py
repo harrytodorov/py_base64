857e4c7d83b78e90593bce15f6039061cc8b5f8c
harry <harry@aidreem.info>
2016-04-28 17:20:00 +0200

    Added rudimental decode function

--- base64.py
+++ base64.py
@@ -40,6 +40,29 @@ def encode_base64(inp):
     return out
 
 
+def decode_base64(inp):
+    """
+    Decode a base64 input string
+    :param inp: The base64 input string
+    :return: Decoded string
+    """
+    out = ''
+    res = ''
+
+    for a in inp:
+        if not int(format(alphabet.find(a))) == 64:
+            out += str(format(alphabet.find(a), '06b'))
+
+    i = len(out) % 8
+    out += i * '0'
+
+    for a in xrange(0, len(out), 8):
+        pair = out[a:a+8]
+        if not pair == ('0' * 8):
+            res += ('%x' % int(pair, 2)).decode('hex').decode('utf-8')
+    return res
+
+
 def test_functions():
     """
     This function is supposed to test the functionality of the functionst provided inside this
@@ -63,6 +86,8 @@ def test_functions():
     text = """Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure."""
     encoded_text = """TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="""
     assert encode_base64(text) == encoded_text
+
+    print decode_base64('YW55IGNhcm5hbCBwbGVhcw==')
 
     print "Tests: Successful."
 
